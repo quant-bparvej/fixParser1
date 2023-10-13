f1c4a08cdc7b08bedc1e2b9a3b129bfc037bfb52
Parvej Bapary <bparvej@gmail.com>
2023-10-13 22:31:25 +0600

    Migration File Added

---
+++ models/FixMessage.js
@@ -0,0 +1,100 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/config.json'); // Replace with the path to your Sequelize database configuration
+
+const FixMessage = sequelize.define('FixMessage', {
+  order_symbol: {
+    type: DataTypes.STRING,
+  },
+  board_type: {
+    type: DataTypes.STRING,
+  },
+  order_status: {
+    type: DataTypes.STRING,
+  },
+  order_qty: {
+    type: DataTypes.INTEGER,
+  },
+  order_rate: {
+    type: DataTypes.FLOAT,
+  },
+  order_yield: {
+    type: DataTypes.INTEGER,
+  },
+  exec_type: {
+    type: DataTypes.STRING,
+  },
+  order_side: {
+    type: DataTypes.STRING,
+  },
+  time_in_force: {
+    type: DataTypes.STRING,
+  },
+  error_msg: {
+    type: DataTypes.STRING,
+  },
+  orderid: {
+    type: DataTypes.STRING,
+  },
+  client_bo: {
+    type: DataTypes.STRING,
+  },
+  engineid: {
+    type: DataTypes.STRING,
+  },
+  reforderid: {
+    type: DataTypes.STRING,
+  },
+  leaves_qty: {
+    type: DataTypes.INTEGER,
+  },
+  cum_qty: {
+    type: DataTypes.INTEGER,
+  },
+  last_qty: {
+    type: DataTypes.INTEGER,
+  },
+  last_px: {
+    type: DataTypes.FLOAT,
+  },
+  avg_px: {
+    type: DataTypes.FLOAT,
+  },
+  min_qty: {
+    type: DataTypes.STRING,
+  },
+  drip_qty: {
+    type: DataTypes.INTEGER,
+  },
+  order_type: {
+    type: DataTypes.STRING,
+  },
+  broker_workstation_id: {
+    type: DataTypes.STRING,
+  },
+  exch_time: {
+    type: DataTypes.DATE,
+  },
+  broker_time: {
+    type: DataTypes.DATE,
+  },
+  latency: {
+    type: DataTypes.STRING,
+  },
+  trade_match_id: {
+    type: DataTypes.STRING,
+  },
+  trade_date: {
+    type: DataTypes.DATE,
+  },
+  settle_date: {
+    type: DataTypes.DATE,
+  },
+  gross_trade_amt: {
+    type: DataTypes.FLOAT,
+  },
+  agressor_indicator: {
+    type: DataTypes.STRING,
+  },
+});
+
+module.exports = FixMessage;
--- processDB.js
+++ processDB.js
@@ -1,6 +1,8 @@
 const sqlite3 = require("sqlite3");
 const { Op } = require("sequelize");
 const Log = require("./models/Logs"); // Adjust the path as needed
+const FixMessage = require('./models/FixMessage'); // Adjust the path as needed
+
 
 async function ConnctingToDatabase() {
   try {
@@ -13,31 +15,41 @@ async function ConnctingToDatabase() {
           return false;
         } else {
           console.log("Database opened");
-          
         }
         //return db;
       }
     );
 
-      const query = "SELECT * FROM logs WHERE prog='fix_engine' and msg like 'Processed Message:%' AND msg NOT LIKE '%Received FIX MSG:%' LIMIT 1000"
-      db.all(query, [], (err, rows) => {
-        if (err) {
-          console.error("Error executing query:", err.message);
-        } else {
-          //console.log("Query result:", rows);
-          rows.forEach((row) => {
-            const msgData = row.msg; // Get the data from the msg column
-            const messageData = JSON.parse(row.msg.replace('Processed Message: {', '{'));
-            console.log('Message Data:', JSON.stringify(messageData, null, 2) + '\n');
-
-          });
-
-
-
-        }
-      });
-
-    
+    const query =
+      "SELECT * FROM logs WHERE prog='fix_engine' and msg like 'Processed Message:%' AND msg NOT LIKE '%Received FIX MSG:%' LIMIT 1000";
+    db.all(query, [], (err, rows) => {
+      if (err) {
+        console.error("Error executing query:", err.message);
+      } else {
+        //console.log("Query result:", rows);
+        rows.forEach((row) => {
+          const msgData = row.msg; // Get the data from the msg column
+          const messageData = JSON.parse(
+            row.msg.replace("Processed Message: {", "{")
+          );
+          console.log(
+            "Message Data:",
+            JSON.stringify(messageData.trade_date, null, 2) + "\n"
+          );
+          // Create a new FixMessage record with the parsed messageData
+          FixMessage.create(messageData)
+            .then((newFixMessage) => {
+              console.log(
+                "New FixMessage record created:",
+                newFixMessage.toJSON()
+              );
+            })
+            .catch((error) => {
+              console.error("Error creating FixMessage record:", error);
+            });
+        });
+      }
+    });
 
     //closing db
     db.close((err) => {
@@ -51,6 +63,13 @@ async function ConnctingToDatabase() {
     console.error("Error connecting to database:", error);
   }
   return true;
+}
+
+async function insertToDB(fixMessageObjetct) {
+  try {
+  } catch (ex) {
+    console.log(ex);
+  }
 }
 
 ConnctingToDatabase();
